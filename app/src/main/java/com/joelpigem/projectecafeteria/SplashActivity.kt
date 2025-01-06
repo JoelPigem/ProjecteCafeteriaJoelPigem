e572e618147256ade401b7de39c5b2487797b08d
JoelPigem <joel.pigem@institutvidreres.cat>
2025-01-06 17:35:56 +0100

    SplashScreen

---
+++ app/src/main/java/com/joelpigem/projectecafeteria/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.joelpigem.projectecafeteria
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.joelpigem.projectecafeteria.Login.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
